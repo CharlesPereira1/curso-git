34d89279ac03d0927c3fac2e1ee1aae2983b1f81
Charles Pereira <charlesti.ptu@gmail.com>
2019-08-16 20:46:40 -0300

    Desejando um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -5,6 +5,8 @@ namespace projeto_teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+
+            Console.WriteLine("Tenha um bom dia!");
         }
     }
 }
